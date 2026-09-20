1ab60c117a6c5b03bd81eb2b52c9047b1102eb9a
Olga <anandaolga410@gmail.com>
2026-09-21 01:08:26 +0700

    setting vercel lagi bs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -26,4 +27,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
+
+        if (env('APP_DEBUG_RAW')) {
+            $exceptions->render(fn (Throwable $e) => new Response(
+                'RAW: '.get_class($e).': '.$e->getMessage()."\n".$e->getFile().':'.$e->getLine()."\n\n".$e->getTraceAsString(),
+                500,
+                ['Content-Type' => 'text/plain']
+            ));
+        }
     })->create();
