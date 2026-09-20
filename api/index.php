b0f8fc2914bbcefe18cd255a19dab86b5605a3a5
Olga <anandaolga410@gmail.com>
2026-09-21 00:39:43 +0700

    setting vercel lagi lagi

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+try {
+    require __DIR__.'/../public/index.php';
+} catch (Throwable $e) {
+    http_response_code(500);
+    header('Content-Type: text/plain');
+    echo 'DIAGNOSTIC: '.get_class($e).': '.$e->getMessage()."\n";
+    echo $e->getFile().':'.$e->getLine()."\n\n";
+    echo $e->getTraceAsString();
+}
